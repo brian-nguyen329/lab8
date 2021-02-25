85f792e1a399b54400a605772d845377e306643a
brian-nguyen329 <brnguyen@ucsd.edu>
2021-02-25 10:36:32 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'm5E6AU0RDV1CtN1ewT01GlZOLaufEFXf';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878481, - 117.235814],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.878481, -117.235814]).addTo(map);
 }
